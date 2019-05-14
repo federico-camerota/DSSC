8df8cae89631c578b4440f6fe02e3af80c038b18
Federico Camerota <fedes770@gmail.com>
2019-05-14 16:33:29 +0200

    Fixed cuda fast transpose

--- homeworks/03_cuda/01_transpose/fast_transpose.cu
+++ homeworks/03_cuda/01_transpose/fast_transpose.cu
@@ -1,13 +1,19 @@
 #include <math.h>
 #include <stdio.h>
+#include <sys/time.h>
+#include <sys/resource.h>
 
-# define N 128
-# define N_THRDS 16
+# define N 8192
+# define N_THRDS 1024
+
 
 __global__ void init_mat (size_t *A_array, size_t **A, const size_t cols){
 
     size_t i = threadIdx.x;
-    A[i] = (A_array + i*cols);
+    while ( i < cols){
+	A[i] = (A_array + i*cols);
+	i += blockDim.x;
+    }
 }
 
 __global__ void transpose (size_t **A, size_t **B, const size_t n){
@@ -26,29 +32,19 @@ __global__ void fast_transpose(size_t **A, size_t **B, const size_t dim){
     __shared__ size_t a_block[N_THRDS];
     __shared__ size_t b_block[N_THRDS];
 
-    if (threadIdx.x == 0 && threadIdx.y == 0){
-    
-    size_t i;
-    for (i = 0; i < N_THRDS; ++i)
-        a_block[i] = A[dim*blockIdx.y + i/dim][dim*blockIdx.x + i%dim];
-    }
+    size_t i = threadIdx.y*dim + threadIdx.x;
+    a_block[i] = A[dim*blockIdx.y + i/dim][dim*blockIdx.x + i%dim];
+
     __syncthreads();
 
     b_block[dim*threadIdx.y + threadIdx.x] = a_block[dim*threadIdx.x + threadIdx.y];
-
-    __syncthreads();
-
-    if(threadIdx.x == 0 && threadIdx.y == 0){
-    
-    size_t i;
-    for (i = 0; i < N_THRDS; ++i)
-        B[dim*blockIdx.x + i/dim][dim*blockIdx.y + i%dim] = b_block[i];
-    }
+    B[dim*blockIdx.x + i/dim][dim*blockIdx.y + i%dim] = b_block[i];
 }
 
 void fill_mat(size_t *mat, const size_t rows, const size_t cols);
 int is_transpose(size_t *mat, size_t *transp, const size_t n);
 void print_is_transpose(size_t *mat, size_t *transp, const size_t n);
+double seconds();
 
 int main() {
 
@@ -67,14 +63,18 @@ int main() {
 
   cudaMemcpy( dev_mat_array, mat_array, N*N*sizeof(size_t), cudaMemcpyHostToDevice ); 
 
-  init_mat<<< 1, N >>>(dev_mat_array, dev_mat,N);
-  init_mat<<< 1, N >>>(dev_transp_array, dev_transp,N);
+  init_mat<<< 1, 1024 >>>(dev_mat_array, dev_mat,N);
+  init_mat<<< 1, 1024 >>>(dev_transp_array, dev_transp,N);
 
-  transpose<<<N, N>>>(dev_mat, dev_transp, N);
+  double start, elapsed;
+  start = seconds();
+  transpose<<<N, 1024>>>(dev_mat, dev_transp, N);
+  cudaDeviceSynchronize();
+  elapsed = seconds() - start;
 
   cudaMemcpy( transp_array, dev_transp_array, N*N*sizeof(size_t),   cudaMemcpyDeviceToHost );
 
-  printf("Transpose result is: %d\n", is_transpose(mat_array, transp_array, N));
+  printf("Transpose result is: %d (%lf seconds)\n", is_transpose(mat_array, transp_array, N), elapsed);
 
  size_t dim= (size_t)sqrt(N_THRDS);
   dim3 grid,block;
@@ -83,11 +83,14 @@ int main() {
   block.x=dim;
   block.y=dim;
 
+  start = seconds();
   fast_transpose<<< grid, block >>>(dev_mat, dev_transp,dim);
+  cudaDeviceSynchronize();
+  elapsed = seconds() - start;
 
   cudaMemcpy( transp_array, dev_transp_array, N*N*sizeof(size_t),   cudaMemcpyDeviceToHost );
 
-  printf("Fast transpose result is: %d\n", is_transpose(mat_array, transp_array, N));
+  printf("Fast transpose result is: %d (%lf seconds)\n", is_transpose(mat_array, transp_array, N), elapsed);
 
 //  printf("Matrix:\n");
 //  size_t i;
@@ -136,4 +139,19 @@ void print_is_transpose(size_t *mat, size_t *transp, const size_t n){
         printf("%d",(mat[i*n + j] != transp[j*n + i]) ? 0 : 1);
     putchar('\n');
     }
+}
+
+double seconds()
+
+/* Return the second elapsed since Epoch (00:00:00 UTC, January 1, 1970)                                                                     
+ */
+
+{
+
+  struct timeval tmp;
+  double sec;
+  gettimeofday( &tmp, (struct timezone *)0 );
+  sec = tmp.tv_sec + ((double)tmp.tv_usec)/1000000.0;
+  return sec;
+
 }
